700ef7cf4bf69af20e109a2b7b795f2eeda1a6b7
Sarah Beck <sarah.lizzie.beck@gmail.com>
2020-06-03 18:35:18 -0600

    added quotes, added small css change to contact form

--- public_html/src/index.js
+++ public_html/src/index.js
@@ -5,46 +5,40 @@ const bodyParser = require('body-parser')
 const {check, validationResult} = require("express-validator")
 const Recaptcha = require('express-recaptcha').RecaptchaV2
 
-// Initializing Express Application
+// initializing the express application
 const app = express()
 
-// Making express aware of project-wide middleware
+// project wide middleware declarations for Express
 app.use(morgan('dev'))
-app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
-
 const recaptcha = new Recaptcha(process.env.RECAPTCHA_SITE_KEY, process.env.RECAPTCHA_SECRET_KEY)
 const indexRoute = express.Router()
 
 const requestValidation = [
-	check("email", "A valid email is required".isEmail().normalizeEmail(),
-	check ('name', 'A name is required to send an email').not().isEmpty().trim().escape(),
-	check ('subject').optional().trim().escape(),
-	check('message', 'A message is required to send email.').not().isEmpty().trim().escape().isLength({max:2000}))
+	check("email", "A Valid Email is required").isEmail().normalizeEmail(),
+	check('name', "A name is required to send an email").not().isEmpty().trim().escape(),
+	check('subject').optional().trim().escape(),
+	check('message', 'A message is required to send email').not().isEmpty().trim().escape().isLength({max: 2000})
+
 ]
 
 indexRoute.route('/apis')
+
+
+	// quick fix to something stupid @gkephart did
 	.get((request, response) => {
-<<<<<<< HEAD
 		return response.json("Is this thing on?")
 	}).post(recaptcha.middleware.verify, requestValidation, (request, response) => {
 
-	response.append('Content-Type', 'text/html')
+	// response.append('Content-Type', 'text/html')
 
 	if (request.recaptcha.error) {
-		return response.send(`<div class='alert alert-danger' role='alert'>There was an error with Recaptcha. Please try again later.</div>`)
+		return response.send(`<div class='alert alert-danger' role='alert'>There was an error with Recaptcha, try again later.</div>`)
 	}
-=======
-	return response.json("Testing A")
-	})
-	.post( requestValidation, (request, response) => {
-		const domain = process.env.MAILGUN_DOMAIN
-		const mg = mailgun({apiKey: process.env.MAILGUN_API_KEY, domain: domain});
->>>>>>> 0675cfe537877810324eebc6f56bba81696afae9
 
 	const errors = validationResult(request)
 
-<<<<<<< HEAD
 	if (!errors.isEmpty()) {
 		const currentError = errors.array()[0]
 		return response.send(Buffer.from(`<div class='alert alert-danger' role='alert'>${currentError.msg}</div>`))
@@ -53,20 +47,6 @@ indexRoute.route('/apis')
 	const domain = process.env.MAILGUN_DOMAIN
 	const mg = mailgun({apiKey: process.env.MAILGUN_API_KEY, domain: domain});
 	const {email, subject, name, message} = request.body
-=======
-		const mailgunData = {
-			to: process.env.MAIL_RECIPIENT,
-			from: `Mailgun Sandbox <postmaster@${domain}>`,
-			subject: `${name} - ${email}`,
-			text: message
-		};
-
-		mg.messages().send(mailgunData, (error) => {
-			if (error) {return response.json("error sending email through email handler, please try again later")}
-		})
-
-		const errors = validationResult(request)
->>>>>>> 0675cfe537877810324eebc6f56bba81696afae9
 
 	const mailgunData = {
 		to: process.env.MAIL_RECIPIENT,
@@ -77,7 +57,7 @@ indexRoute.route('/apis')
 
 	mg.messages().send(mailgunData, (error) => {
 		if (error) {
-			return response.send(Buffer.from(`<div class='alert alert-danger' role='alert'>Unable to send email error with email sender, please try again later.</div>`))
+			return response.send(Buffer.from(`<div class='alert alert-danger' role='alert'>Unable to send email error with email sender, try again later.</div>`))
 		}
 	})
 
